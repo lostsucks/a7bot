ecbcf23ae12e42fac2ca5b6b643b2518bb2824ac
Elisha <lostmatterlmao@gmail.com>
2024-01-21 12:47:42 -0600

    Removed unneeded comments.

--- commands/giveaways/giveawaylist.js
+++ commands/giveaways/giveawaylist.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
-const { giveawayData } = require('./giveawaystorage'); // Adjust the path accordingly
+const { giveawayData } = require('./giveawaystorage');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,19 +8,16 @@ module.exports = {
         .setDescription('Lists all active giveaway IDs.')
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
     async execute(interaction) {
-        // Fetch the active giveaway IDs
         const activeGiveaways = Object.entries(giveawayData)
             .filter(([id, data]) => !data.ended)
             .map(([id]) => id);
         
-        // Create an embed with the list of giveaway IDs
         const giveawayListEmbed = new EmbedBuilder()
             .setColor(0x0099ff)
             .setTitle('Active Giveaways')
             .setDescription(activeGiveaways.length > 0 ? activeGiveaways.join('\n') : 'No active giveaways at the moment.')
             .setTimestamp();
 
-        // Reply with the embed
         await interaction.reply({ embeds: [giveawayListEmbed] });
     },
 };
